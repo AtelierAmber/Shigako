78868e819993d47651d94d5136fbfb1ef8b24568
Essc0de <esscode@gmail.com>
2016-11-30 15:43:27 -0700

    Setup Widget Files

---
+++ Shigako/Layers.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Engine.h"
+
+//Available functions
+/*this->addButton(ButtonPic.png, Location, Text, vec2 size)
+ *this->addImage(Image.png, Location, vec2 size)
+ *this->addSlider(Line.png, Slide.png, Location, vec2 size, min, max)
+ *
+ *Location is defined as either LocationLayout::TOP, ::MID, ::LEFT, ::RIGHT, ::BOT
+ *or a vec2 of x, y in relation to the top left of the widget box
+ *
+ *vec2 is defined as vec2(x, y); It's just a x and y position
+ */
+
+class Layers{
+  public:
+  
+  private:
+  
+}
