5a0418ad9bf58480e3f21e07be3dff0178d4176c
tnewton100 <thenewtons100@hotmail.com>
2017-01-25 11:27:19 -0700

    fixed sliders

--- Shigako/ColorPicker.cpp
+++ Shigako/ColorPicker.cpp
@@ -8,6 +8,17 @@ ColorPicker::ColorPicker(Engine *parent)
 	color=addImage("Assets/Blank.png",Location(0,0,3,3));
 	addSlider(0,255,&r,true, Location(0,3,1,1),[this](){
 		color->setColor(qRgba(r,g,b,255));
+		m_engineParent->setPenColor(qRgba(r,g,b,255));
+	});
+	
+	addSlider(0,255,&g,true, Location(1,3,1,1),[this](){
+		color->setColor(qRgba(r,g,b,255));
+		m_engineParent->setPenColor(qRgba(r,g,b,255));
+	});
+	
+	addSlider(0,255,&b,true, Location(2,3,1,1),[this](){
+		color->setColor(qRgba(r,g,b,255));
+		m_engineParent->setPenColor(qRgba(r,g,b,255));
 	});
 }
 ColorPicker::~ColorPicker(){
